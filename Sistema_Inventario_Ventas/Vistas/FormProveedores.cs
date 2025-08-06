43696b8c30e3342a2776a62dab28782b8cd5e881
Jhon Eduard <eduardcamayo13@gmail.com>
2025-08-05 19:48:54 -0500

    Agregando pequeños cambios

--- Sistema_Inventario_Ventas/Vistas/FormProveedores.cs
+++ Sistema_Inventario_Ventas/Vistas/FormProveedores.cs
@@ -41,6 +41,50 @@ namespace Sistema_Inventario_Ventas.Vistas
             string email = tbEmail.Text;
 
             Proveedor nuevoProveedor = new Proveedor(nomProveedor, numContacto, direccion, email);  
+            string resultado = metodosProveedores.ValidarProveedor(nuevoProveedor);
+            if (resultado.Equals("OK"))
+            {
+                resultado = metodosProveedores.AgregarProveedor(nuevoProveedor);
+            }
+            MessageBox.Show(resultado);
+            LimpiarCampos();
+            this.datos.Reset();
+            this.MostrarProveedores();
+            nuevoProveedor = null;
+
+        }
+        private void btnBuscarProveedor_Click(object sender, EventArgs e )
+        {
+            this.datos.Reset();
+            metodosProveedores = new MetodosProveedores();
+            datos = metodosProveedores.BuscarProveedor(tbBuscarUsuario.Text.Trim());
+            FormatoTablaProveedores(datos);
+            LimpiarCampos();
+        }
+        private void btnMostrarProveedores_Click(object sender, EventArgs e)
+        {
+            this.LimpiarCampos();
+            this.datos.Reset();
+            this.MostrarProveedores();
+        }
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string nomProveedor = tbProveedor.Text;
+            string numContacto = tbNumero.Text;
+            string direccion = tbDireccion.Text;
+            string email = tbEmail.Text;
+
+            Proveedor actualizarProveedor = new Proveedor(nomProveedor, numContacto, direccion, email);
+            string resultado = metodosProveedores.ValidarProveedor(actualizarProveedor);
+            if (resultado.Equals("OK"))
+            {
+                resultado = metodosProveedores.ActualizarProveedor(idProveedor, actualizarProveedor);
+            }
+            MessageBox.Show(resultado);
+            LimpiarCampos();
+            this.datos.Reset();
+            this.MostrarProveedores();
+            actualizarProveedor = null;
 
         }
         private void FormatoTablaProveedores(DataTable datos)
